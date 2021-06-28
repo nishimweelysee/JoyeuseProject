52b024627c5c09178375a459b4b5b40bd4ce6ec8
nellyganza <nishimwelys@gmail.com>
2021-06-28 17:45:01 +0200

    Intial commit

---
+++ src/main/java/com/elysee/dukachallenge/DukaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.elysee.dukachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elysee/dukachallenge/DukaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysee.dukachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DukaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
